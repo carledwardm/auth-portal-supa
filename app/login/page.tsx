588322ee0b45c5d93d434072c6a0069b87c22a4b
carledwardm <cmillardv77@gmail.com>
2026-02-10 17:18:06 -0500

    Add form controlled inputs and placeholder logic for submit function

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import styles from "./login.module.scss";
 
 export default function login() {
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,15 +1,27 @@
+"use client";
 import styles from "./signup.module.scss";
+import { useState } from "react";
 
 export default function signup() {
+    const [ username, setUsername ] = useState<string>("");
+    const [ email, setEmail ] = useState<string>("");
+    const [ bio, setBio ] = useState<string>("");
+    const [ password, setPassword ] = useState<string>("");
+
+    const handleSubmit = (e: React.SubmitEvent) => {
+        e.preventDefault();
+        console.log(username, email, bio, password);
+    }
+
     return (
         <main className={styles.signupMain}>
                 <section className={styles.signupSection}>
                     <h1 className={styles.signupTitle}>Sign Up</h1>
-                    <form className={styles.signupForm}>
-                        <input type="text" className={styles.input} placeholder="Username"></input>
-                        <input type="text" className={styles.input} placeholder="Email"></input>
-                        <textarea className={styles.bioInput} placeholder="Bio"></textarea>
-                        <input type="password" className={styles.input} placeholder="Password"></input>
+                    <form className={styles.signupForm} onSubmit={handleSubmit}>
+                        <input type="text" className={styles.input} placeholder="Username" onChange={ e => setUsername(e.target.value)}></input>
+                        <input type="text" className={styles.input} placeholder="Email" onChange={ e => setEmail(e.target.value)}></input>
+                        <textarea className={styles.bioInput} placeholder="Bio" onChange={ e => setBio(e.target.value)}></textarea>
+                        <input type="password" className={styles.input} placeholder="Password" onChange={ e => setPassword(e.target.value)}></input>
                         <button type="submit" className={styles.submitBtn}>Submit</button>
                     </form>
             </section>
