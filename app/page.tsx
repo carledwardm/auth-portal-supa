239354f1096c4dbb4e4a188f15c9b9566943604f
carledwardm <cmillardv77@gmail.com>
2026-02-11 18:20:02 -0500

    Add dynamic rendering of homepage title to display user first name

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+'use client';
 import styles from "./page.module.css";
+import { useSupa } from "@/context/SupaContext";
 
 export default function Home() {
+  const { user } = useSupa();
+  console.log(user);
+
   return (
       <main className={styles.main}>
-        <h1>Temporary Heading!</h1>
+        <h1 className={styles.userGreeting}>Hello, <span className={styles.userName}>{user?.user_metadata.first_name}</span>!</h1>
       </main>
   );
 }
