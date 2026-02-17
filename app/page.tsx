9a66e9e68af91ad17b9bab217bbad0c2064f9f77
carledwardm <cmillardv77@gmail.com>
2026-02-17 17:38:10 -0500

    Add log-in message on homepage for unauthorized users

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,8 @@ export default function Home() {
 
   return (
       <main className={styles.main}>
-        <h1 className={styles.userGreeting}>Hello, <span className={styles.userName}>{user?.user_metadata.first_name}</span>!</h1>
+        {!user && <h1>Hello, please log in!</h1>}
+        {user && <h1 className={styles.userGreeting}>Hello, <span className={styles.userName}>{user?.user_metadata.first_name}</span>!</h1>}
       </main>
   );
 }
