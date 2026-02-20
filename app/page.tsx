025f11933d98ee02719a0a5d6f81fc9f81f4742a
carledwardm <cmillardv77@gmail.com>
2026-02-20 12:53:18 -0500

    Add condition to render name 'Guest' on homepage if test account/no name given

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,14 @@ import { useSupa } from "@/context/SupaContext";
 export default function Home() {
   const { user } = useSupa();
 
-
+  console.log( user )
 
   return (
       <main className={styles.main}>
         {!user && <h1>Hello, please log in!</h1>}
         {user && <h1 className={styles.userGreeting}>Hello, <span className={styles.userName}>
           {
-            user?.user_metadata.first_name || user?.user_metadata.full_name.split(" ")[0]
+            user?.user_metadata.first_name || user?.user_metadata.full_name?.split(" ")[0] || "Guest"
           }
           </span>!</h1>}
       </main>
